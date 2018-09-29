ce238e2b43b0f493f17bd01a116b5ef05d071d2c
Alesya Polovets <alesya.polovets@gmail.com>
2018-09-29 14:10:52 +0300

    longest-consecutive-sequence/feature: implemented function longestConsecutiveLength(array), that returns the length of the longest sequence with step equal to 1 only

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function longestConsecutiveLength(array) {
   // your solution here
-}
+    if (array.length === 0) {
+        return 0;
+    }
+    let result = 1;
+    let count = 1;
+    function compareNumber (a, b) {
+        return a - b;
+    }
+    array.sort(compareNumber);
+    for (let i = 0, length = array.length; i < length; i++) {
+        if (array[i + 1] - array[i] === 1) {
+            count++;
+        }
+        else if (array[i + 1] - array[i] > 1) {
+            result = Math.max(result, count);
+            count = 1;
+        }
+    }
+    return result;
+};
